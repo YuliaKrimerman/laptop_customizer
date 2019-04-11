ba3cfe45dbdb890704f4f1d697a8a0e005b944dd
YuliaKrimerman <46899367+YuliaKrimerman@users.noreply.github.com>
2019-04-11 09:31:15 -0400

    For Grade

---
+++ src/Features.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import FeatureList from './FeatureList'
+
+function Features(props){
+
+return (
+
+    <section className="main__form">
+            <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
+            <FeatureList 
+            features ={props.features}
+            handleUpdate={props.handleUpdate}
+            selected ={props.selected}
+        />
+          </section>
+    )
+}
+export default Features;
